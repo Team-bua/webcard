7826435b495f0dadd4f36a1f334a5489a8f980a2
blackbullerk123 <65282322+blackbullerk123@users.noreply.github.com>
2021-08-15 20:43:10 +0700

    Create 2021_08_15_203531_create_index_table.php

---
+++ database/migrations/2021_08_15_203531_create_index_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIndexTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('index', function (Blueprint $table) {
+            $table->id();
+            $table->text('image_logo')->nullable();
+            $table->text('image_banner')->nullable();
+            $table->string('desc_banner')->nullable();
+            $table->string('sub_desc_banner')->nullable();
+            $table->string('title_serve')->nullable();
+            $table->string('icon_serve')->nullable();
+            $table->string('desc_serve')->nullable();
+            $table->string('image_step')->nullable();
+            $table->string('desc_step')->nullable();
+            $table->string('sub_desc_step')->nullable();
+            $table->string('desc_number_step')->nullable();
+            $table->string('sub_desc_number_step')->nullable();
+            $table->string('image_backgound')->nullable();
+            $table->string('decs_to_do')->nullable();
+            $table->string('sub_desc_to_do')->nullable();
+            $table->string('icon_to_do')->nullable();
+            $table->string('desc_icon_to_do')->nullable();
+            $table->string('sub_desc_icon_to_do')->nullable();
+            $table->string('image_to_do')->nullable();
+            $table->string('desc_contact')->nullable();
+            $table->string('phone_contact')->nullable();
+            $table->string('address_contact')->nullable();
+            $table->string('email_contact')->nullable();
+            $table->string('banner_buy_card')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('index');
+    }
+}
